88e23e84aecb9fc8aae679dfed25e6fe2aa62393
Marc Edees <Marc@unknown.home>
2019-06-08 12:20:29 +0100

    Updates

---
+++ precache-manifest.70760cb699399ecaf2062080ddaae218.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9221180852b653c1c9796418e2a0e10b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9941d9dae77b23ca7eb8",
+    "url": "/static/css/main.2464d45d.chunk.css"
+  },
+  {
+    "revision": "991bcd1fe02eb18ef00b",
+    "url": "/static/js/2.c174cbbf.chunk.js"
+  },
+  {
+    "revision": "9941d9dae77b23ca7eb8",
+    "url": "/static/js/main.46fb5d8b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e19d3dc8d1f24178a637cd9cb1e12a15",
+    "url": "/static/media/backg.e19d3dc8.jpg"
+  }
+]);
