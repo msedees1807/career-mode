bbb87408b99c6c64e78574ef3ce93541382b9099
Marc Edees <marc_edees@hotmail.co.uk>
2020-10-16 22:59:01 +0100

    uodates to result mechanic

--- src/components/newResult.js
+++ src/components/newResult.js
@@ -6,22 +6,24 @@ export default function NewResult() {
     const [rounds, updateRounds] = useState(4);
 
     function updateMessage(x){
-        newMessage(messages.concat(Math.floor(Math.random() * 2) === 0 ? "Talks progressing" : "Talks ended"))
+        newMessage(messages.concat(Math.floor(Math.random() * 2) === 0 ? "Talks progress" : "Talks move backwards"))
      updateRounds(rounds - 1)
     }
 
     function clearMessage(){
         newMessage([])
+        updateRounds(4)
     }
 
     return (
         <div>
-            {rounds > 0 ? <button onClick={()=> updateMessage(3)}>Test</button> :
-            <>Nahhhh</>}
+            {rounds > 0 ? <button onClick={()=> updateMessage(3)}>Next offer</button> :
+            <button disabled>Test</button>}
             <button onClick={()=> clearMessage()}>Clear</button>
             {messages && messages.map((x)=>(
                 <p>{x}</p>
             ))}
+            {rounds === 0 && <>Talks have concluded. Verdict: {messages.reduce((count, result) => result ==="Talks progress" && count + 1) > messages.length ? "The player will be sold" : "Talks failed"}</>}
         </div>
     )
 }
