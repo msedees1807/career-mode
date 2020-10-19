8eaed13db514fa81742b8d02ac392b36ce5175f4
Marc Edees <marc_edees@hotmail.co.uk>
2020-10-19 21:00:54 +0100

    new text messages

--- src/components/newResult.js
+++ src/components/newResult.js
@@ -1,12 +1,23 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import styled from "styled-components/macro";
+import {news} from "../data/news"
 
 export default function NewResult() {
+
+    const TextMessage = styled.p`
+    color: black;
+    background-color: white;
+    border-radius: 30px;
+    font-size: 16px;
+    text-align: left;
+    padding: 10px;
+    `
 
     const [messages, newMessage] = useState([]);
     const [rounds, updateRounds] = useState(4);
 
     function updateMessage(x){
-        newMessage(messages.concat(Math.floor(Math.random() * 2) === 0 ? "Talks progress" : "Talks move backwards"))
+        newMessage(messages.concat(Math.floor(Math.random() * 2) === 0 ? news[0].news + " Talks progress" : news[1].news +  "Talks move backwards"))
      updateRounds(rounds - 1)
     }
 
@@ -17,10 +28,10 @@ export default function NewResult() {
 
     function calculateVerdict(){
     if(messages.reduce((count, result) => result === "Talks progress" && count + 1) > 0){
-         return "The player will be sold"
+         return "A deal has been agreed. The player will be sold."
         }
         else {
-         return "Talks failed" 
+         return "Talks failed. The player will remain at the club." 
         }
     }
     
@@ -28,17 +39,13 @@ export default function NewResult() {
     return (
         <div>
             {rounds > 0 ? <button onClick={()=> updateMessage(3)}>Next offer</button> :
-            <button disabled>Test</button>}
+            <button disabled>Next offer</button>}
             <button onClick={()=> clearMessage()}>Clear</button>
             {messages && messages.map((x)=>(
-                <p>{x}</p>
+                <TextMessage>{x}</TextMessage>
             ))}
-            {/* {rounds === 0 && calculateVerdict() <>Talks have concluded. Verdict: {messages.reduce((count, result) => result ==="Talks progress" && count + 1) > messages.length ? "The player will be sold" : "Talks failed"}</>} */}
         
-            
-            {rounds === 0 && <>Talks have concluded.<br/> Verdict:</>}
-            {rounds === 0 && calculateVerdict()}
-
+            {rounds === 0 && "Verdict:" && calculateVerdict()}
         
         </div>
     )
--- src/data/news.js
+++ src/data/news.js
@@ -1,25 +1,30 @@
 export const news = [
     { 
+        id: 1,
         news: "The CEO has arrived for discussions via private jet.",
         step: 1,
         outcome: "sell"
     },
     { 
+        id: 2,
         news: "The CEO has been delayed for discussions by the Coronavirus.",
         step: 1,
         outcome: "keep"
     },
     { 
+        id: 3,
         news: "The CEO has arrived for discussions by bicycle. The club are participating in a \"Green\" transfer window.",
         step: 1,
         outcome: "sell"
     },
     { 
+        id: 4,
         news: "A starting offer has been made. The board laugh.",
         step: 2,
         outcome: "keep"
     },
     { 
+        id: 5,
         news: "A starting offer has been made. The board look pleased.",
         step: 2,
         outcome: "sell"
