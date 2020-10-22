5cafad0a1cf9bd939dcf5f464d3efdf2c81d458d
Marc Edees <marc_edees@hotmail.co.uk>
2020-10-22 15:12:57 +0100

    updated tests

--- src/components/utils.test.js
+++ src/components/utils.test.js
@@ -1,24 +1,41 @@
 import { calculateNegotiationResult } from './utils';
 import { playerSoldMessage, playerStayingMessage } from './../data/messages';
 
-test('Calculate result with all sold-supporting events gives sold result', () => {
+test('Calculate final result where all sold-supporting events gives sold result', () => {
     const events = [
         "Test message Talks progress",
         "Message stating Talks progress",
         "Talks progress",
         "Test Talks progress"
     ];
-
     expect(calculateNegotiationResult(events)).toBe(playerSoldMessage);
 });
 
-test('Calculate result with all stay-supporting events gives stay result', () => {
+test('Calculate result where all stay-supporting events gives stay result', () => {
     const events = [
         "Test message talks not progress",
         "Message",
         "Bad message",
         "Valid but wrong message"
     ];
-
     expect(calculateNegotiationResult(events)).toBe(playerStayingMessage);
 });
+
+test('Calculate result when given 2 progress events', ()=>{
+    const events = [
+        "Test message Talks progress",
+        "Message",
+        "Test message Talks progress",
+        "Message"
+    ];
+    expect(calculateNegotiationResult(events)).toBe(playerSoldMessage);
+})
+
+test('Calculate result when given 1 progress events', ()=>{
+    const events = [
+        "Test message Talks progress",
+        "Message",
+        "Message"
+    ];
+    expect(calculateNegotiationResult(events)).toBe(playerStayingMessage);
+})
